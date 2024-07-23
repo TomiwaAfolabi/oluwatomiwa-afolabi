76cf953589a4684ac10112d4eb368361f202cc39
TomiwaAf <tomiwasanto@gmail.com>
2024-07-23 19:59:40 +0100

    commit

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="mt-10 flex flex-col gap-20 px-4">
     <div
-      class="bg-[#EAE3D2] rounded-xl flex flex-col items-center mx-auto overflow-scroll p-8"
+      class="w-full h-[400px] bg-[#EAE3D2] rounded-xl flex flex-col items-center mx-auto overflow-scroll"
     >
       <div class="mt-2 text-center">
         <div class="flex flex-col gap-4">
-          <p class="font-bold underline mb-2 text-4xl">Projects</p>
-          <project-card />
+          <p class="font-bold underline mb-2">Projects</p>
+          <a
+            class="decoration-inherit cursor-pointer underline hover:text-[#99522C]"
+            href="https://location-tracking-fe.vercel.app/"
+            target="_blank"
+            ><p>Location Tracking Application</p></a
+          >
+          <a
+            class="decoration-inherit cursor-pointer underline hover:text-[#99522C]"
+            href="https://vue-school-project-navy.vercel.app/"
+            target="_blank"
+            ><p>Vue School Project</p></a
+          >
+          <a
+            class="decoration-inherit cursor-pointer underline hover:text-[#99522C]"
+            href="https://rendezvous-eight.vercel.app"
+            target="_blank"
+            ><p>Events Management System</p></a
+          >
+          <a
+            class="decoration-inherit cursor-pointer underline hover:text-[#99522C]"
+            href="https://sensational-torrone-2feab3.netlify.app"
+            target="_blank"
+            ><p>Online Store</p></a
+          >
         </div>
       </div>
     </div>
@@ -14,10 +37,7 @@
 </template>
 
 <script>
-import ProjectCard from "./reusables/ProjectCard.vue";
-export default {
-  components: { ProjectCard },
-};
+export default {};
 </script>
 
 <style></style>
