43b40ee4643d06486bfccf45506912f5bf4c983e
TomiwaAf <tomiwasanto@gmail.com>
2024-07-23 19:53:40 +0100

    some uodates

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,35 +1,12 @@
 <template>
   <div class="mt-10 flex flex-col gap-20 px-4">
     <div
-      class="w-full h-[400px] bg-[#EAE3D2] rounded-xl flex flex-col items-center mx-auto overflow-scroll"
+      class="bg-[#EAE3D2] rounded-xl flex flex-col items-center mx-auto overflow-scroll p-8"
     >
       <div class="mt-2 text-center">
         <div class="flex flex-col gap-4">
-          <p class="font-bold underline mb-2">Projects</p>
-          <a
-            class="decoration-inherit cursor-pointer underline hover:text-[#99522C]"
-            href="https://location-tracking-fe.vercel.app/"
-            target="_blank"
-            ><p>Location Tracking Application</p></a
-          >
-          <a
-            class="decoration-inherit cursor-pointer underline hover:text-[#99522C]"
-            href="https://vue-school-project-navy.vercel.app/"
-            target="_blank"
-            ><p>Vue School Project</p></a
-          >
-          <a
-            class="decoration-inherit cursor-pointer underline hover:text-[#99522C]"
-            href="https://rendezvous-eight.vercel.app"
-            target="_blank"
-            ><p>Events Management System</p></a
-          >
-          <a
-            class="decoration-inherit cursor-pointer underline hover:text-[#99522C]"
-            href="https://sensational-torrone-2feab3.netlify.app"
-            target="_blank"
-            ><p>Online Store</p></a
-          >
+          <p class="font-bold underline mb-2 text-4xl">Projects</p>
+          <project-card />
         </div>
       </div>
     </div>
@@ -37,7 +14,10 @@
 </template>
 
 <script>
-export default {};
+import ProjectCard from "./reusables/ProjectCard.vue";
+export default {
+  components: { ProjectCard },
+};
 </script>
 
 <style></style>
---
+++ components/reusables/ProjectCard.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <NuxtLink
+      v-for="(project, i) in projectsData"
+      :key="i"
+      class="w-full relative overflow-hidden"
+      to=""
+    >
+      <div
+        class="relative bg-white/40 p-6 flex flex-col sm:flex-row gap-8 rounded-none sm:rounded-xl justify-center items-center btn cursor-pointer mb-4"
+      >
+        <div class="w-full h-full absolute hover:opacity-0">
+          <Icon
+            class="w-[60px] h-[60px] relative top-[20%]"
+            :name="project.icon"
+          />
+        </div>
+
+        <div>
+          <img class="max-w-[200px]" :src="imgInfo[0]" alt="project image" />
+        </div>
+
+        <div class="flex flex-col gap-1">
+          <h1 class="text-xl">{{ project.title || "Project" }}</h1>
+          <p class="text-[12px]">{{ project.tools || "Project tools" }}</p>
+        </div>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+import projectsData from "~/static/projects.json";
+export default {
+  data() {
+    return {
+      projectsData,
+    };
+  },
+  computed: {
+    imgInfo() {
+      let imginfo = [];
+      projectsData.map((el) => imginfo.push(el.img.toString()));
+      return imginfo;
+    },
+  },
+};
+</script>
+
+<style>
+.btn:hover {
+  box-shadow: 1px 1px 25px 10px rgba(139, 115, 75, 0.4);
+}
+
+.btn:before {
+  content: "";
+  position: absolute;
+  top: 0%;
+  left: -0%;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(#eae3d2, #eae3d2, #eae3d2);
+  transition: all 200ms;
+}
+
+.btn:hover:before {
+  left: 105%;
+}
+</style>
