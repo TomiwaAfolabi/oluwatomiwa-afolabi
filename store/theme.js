1c7a1b251d9c6ed5e5309cf28e1904575ff90183
TomiwaAf <tomiwasanto@gmail.com>
2024-10-28 19:44:07 +0100

    commit

--- store/theme.js
+++ store/theme.js
@@ -10,9 +10,6 @@ export const theme = createStore({
   },
   mutations: {
     switchTheme(state) {
-      //   const themeId = ["1", "2", "3"];
-      //   let randomNumber = "";
-      //   let randNumCopy = "";
       if (state.themeNo === "" && !state.isTheme) {
         state.isTheme = true;
         state.themeNo = "1";
@@ -20,21 +17,6 @@ export const theme = createStore({
         state.themeNo = "";
         state.isTheme = false;
       }
-
-      //   if (state.isTheme) {
-      //     let randomNo = Math.floor(Math.random() * themeId.length).toString();
-      //     randomNumber = themeId[randomNo];
-
-      //     if (randNumCopy === randomNumber) {
-      //       randomNumber = Math.floor(Math.random() * themeId.length).toString();
-      //       randomNumber = themeId[randomNo];
-      //       randNumCopy = themeId[randomNo];
-      //     }
-      //     if (randNumCopy !== randomNumber) {
-      //       randNumCopy = themeId[randomNo];
-      //       state.themeNo = randomNumber;
-      //     }
-      //   }
     },
   },
 });
